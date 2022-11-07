c3cb022fe1c7e2f9f268b8c7c7c4268dd7d63a43
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2022-11-08 01:27:47 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -3,8 +3,6 @@ function solution(array) {
     let n = parseInt(array.length/2);
 
     return array.sort(function(a, b)  {
-  if(a > b) return 1;
-  if(a === b) return 0;
-  if(a < b) return -1;
-})[n];
+  return b - a;
+}).reverse()[n];
 }
