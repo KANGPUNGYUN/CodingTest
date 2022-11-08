d4c3545eefd2d3f336901202fcb1d735347b91e6
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2022-11-08 23:37:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,6 @@
 function solution(price) {
     if(300000>price && price>=100000){
-        return parseInt(price*95/100);
+        return parseInt(price*0.95);
     } else if(500000>price && price>=300000){
         return parseInt(price*0.9);
     } else if(price>=500000){
