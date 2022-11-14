931dffb3827abe234e639c198df7d10fc9c04381
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2022-11-14 18:03:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,16 +1,8 @@
-function solution(my_string, num1, num2) {
-    const one = my_string[num1];
-    const two = my_string[num2];
-    
+function solution(my_string, num1, num2) {  
     let arr = my_string.split("");
-    
-    for(let i = 0; i < arr.length; i++){
-        if(i === num1){
-            arr[i] = two
-        } else if(i === num2){
-            arr[i] = one
-        }
-    }
+    const temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
 
     return arr.join("");
 }
