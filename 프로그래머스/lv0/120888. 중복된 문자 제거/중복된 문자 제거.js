2c47a06c9bf7415bb5aab28f5bc810f50f28109c
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-04 21:31:45 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,18 +1,4 @@
 function solution(my_string) {
-    let arr = my_string.split("");
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i+1; j < arr.length; j++){
-            if(arr[i] === arr[j]){
-                arr.splice(j, 1);
-            }
-        }
-    }
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i+1; j < arr.length; j++){
-            if(arr[i] === arr[j]){
-                arr.splice(j, 1);
-            }
-        }
-    }
-    return arr.join("")
+    
+    return Array.from(new Set(my_string)).join('')
 }
