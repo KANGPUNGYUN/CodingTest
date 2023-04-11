b6eb935f9e2f4cdacffb5ed46613544525e82d6c
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-11 16:34:57 +0900

    [level 2] Title: 올바른 괄호, Time: 5.19 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+    let count = 0;
+    
+    for(let i of s){
+        i === '(' ? count++ : count--;
+        
+        if(count < 0){
+            return false;
+        }
+    }
+
+    return count === 0 ? true : false;
+}
