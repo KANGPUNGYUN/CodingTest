bcd3255dfe7c2dbb3261aeeee316cb35be61e915
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-19 04:21:16 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 1;
+    for(let i = 1; i<n; i++){
+        answer++;
+        while(answer%3 === 0 || answer.toString().includes('3')){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
