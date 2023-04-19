f14eb6915729497df45bb0df2e6003ca37309b91
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-20 00:25:09 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -0,0 +1,13 @@
+function solution(dots) {
+    let res = 0;
+    if((dots[0][0]-dots[1][0])/(dots[0][1]-dots[1][1]) === (dots[2][0]-dots[3][0])/(dots[2][1]-dots[3][1])){
+        res = 1;
+    }
+    if((dots[0][0]-dots[2][0])/(dots[0][1]-dots[2][1]) === (dots[1][0]-dots[3][0])/(dots[1][1]-dots[3][1])){
+        res = 1;
+    }
+    if((dots[0][0]-dots[3][0])/(dots[0][1]-dots[3][1]) === (dots[2][0]-dots[1][0])/(dots[2][1]-dots[1][1])){
+        res = 1;
+    }
+    return res
+}
