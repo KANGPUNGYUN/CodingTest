3f4e047f02ad148fdf8d1c2d28cb5c2d0e041558
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-22 17:07:14 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let multi = 1;
+    let sum = 0;
+    for(let i = 0; i<num_list.length; i++){
+        multi *= num_list[i];
+        sum += num_list[i];
+    }
+    if(multi < sum*sum){
+        return 1;
+    }else{
+        return 0;
+    }
+}
