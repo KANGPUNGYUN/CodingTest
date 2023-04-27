fc0827d0acbbc8151710c1aec78acc7893826ce0
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-04-28 02:38:02 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,9 @@
+function solution(a, b, c) {
+    if(a===b && b===c && c===a){
+        return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
+    }
+    if(a!==b && b!==c && c!==a){
+        return (a+b+c)
+    }
+    return (a+b+c)*(a*a+b*b+c*c)
+}
