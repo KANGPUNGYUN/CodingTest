a6e2f3d267c530880455ebf7fa99e285255ac48e
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-07-19 02:10:29 +0900

    [Silver IV] Title: 듣보잡, Time: 256 ms, Memory: 28180 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -1,19 +1,11 @@
-const fs = require("fs");
-const { join } = require("path");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let [n, m] = input.shift().split(' ').map(Number);
-let neverHeard = new Set();
-let neverSeen = new Set();
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [nh, ns] = input.shift().split(' ');
+let set = new Set();
 let res = [];
 
 for(let i=0; i<input.length; i++){
-    if(i<n){
-        neverHeard.add(input[i]);
-    }else{
-        neverSeen.add(input[i]);
-    }
+    set.has(input[i]) ? res.push(input[i]) : set.add(input[i])
 }
-neverHeard.forEach(i=>{if(neverSeen.has(i)){res.push(i)}})
+
 res = res.sort();
 console.log(`${res.length}\n${res.join('\n')}`);
