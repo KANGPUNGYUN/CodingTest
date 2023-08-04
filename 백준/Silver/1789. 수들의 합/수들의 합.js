f8dd41ccdcc5bd3caa23b9d5c55fc62f74518654
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2023-08-04 16:37:32 +0900

    [Silver V] Title: 수들의 합, Time: 196 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync("/dev/stdin").toString();
+let sum = 0, cnt = 0;
+
+for(let i =1;;i++){
+    sum += i
+    cnt++;
+    if(sum > input){
+        cnt--;
+        break;
+    }
+}
+
+console.log(cnt);
