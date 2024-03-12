fc9a2b9be390df9f8083b46bd86a79ac03bd8d6d
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-03-13 00:14:20 +0900

    [level 2] Title: 스킬트리, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,12 @@
+function solution(skill, skill_trees) {
+	const regex = new RegExp(`[^${skill.split("")}]`, 'g');
+    let res = 0;
+    for(let i=0; i<skill_trees.length; i++){
+        if(skill_trees[i].replace(regex, "").length === 0){
+            res++
+        } else if(skill.includes(skill_trees[i].replace(regex, "")) && skill[0] === skill_trees[i].replace(regex, "")[0]){
+            res++
+        }
+    }
+    return res
+}
