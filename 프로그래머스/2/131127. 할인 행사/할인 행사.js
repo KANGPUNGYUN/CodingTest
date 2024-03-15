ba76d2aed26b74d0828141a2aa90f89ca8005e03
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-03-16 00:40:19 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -7,16 +7,9 @@ function solution(want, number, discount) {
     map1 = [...map1].sort();
     for(let i=0; i<discount.length-9; i++){
         let map2 = new Map();
-        map2.set(discount[i], 1)
-        map2.set(discount[i+1], map2.get(discount[i+1])+1 || 1)
-        map2.set(discount[i+2], map2.get(discount[i+2])+1 || 1)
-        map2.set(discount[i+3], map2.get(discount[i+3])+1 || 1)
-        map2.set(discount[i+4], map2.get(discount[i+4])+1 || 1)
-        map2.set(discount[i+5], map2.get(discount[i+5])+1 || 1)
-        map2.set(discount[i+6], map2.get(discount[i+6])+1 || 1)
-        map2.set(discount[i+7], map2.get(discount[i+7])+1 || 1)
-        map2.set(discount[i+8], map2.get(discount[i+8])+1 || 1)
-        map2.set(discount[i+9], map2.get(discount[i+9])+1 || 1)
+        for(let j=0; j<10; j++){
+            map2.set(discount[i+j], map2.get(discount[i+j])+1 || 1)
+        }
         map2 = [...map2].sort()
         let id = map2.every((val,index,arr)=>arr[index][0]===map1[index][0] && arr[index][1]===map1[index][1])
         if(id){
