207b0ab3961a7fb4e5b5dca4196569304bd49816
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-03-29 10:55:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.93 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,10 @@
 function solution(arr)
 {
-    var answer = [];
-    
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i] !== arr[i+1]){
-            answer.push(arr[i]);
-        }
+    let res = [arr[0]];
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i] !==arr[i+1]){
+            res.push(arr[i+1])
+        } 
     }
-    
-    return answer;
+    return res 
 }
