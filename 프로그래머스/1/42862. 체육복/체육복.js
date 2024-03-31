06fc1a861b6fd197d65b32f7228b6cf301a3d428
PUNGY <zkdvnd@naver.com>
2024-03-31 14:19:22 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,28 +1,16 @@
 function solution(n, lost, reserve) {
-    let lostMap = new Map();
-    let reserveMap = new Map();
+    [lost, reserve] = [lost.filter(v=>!reserve.includes(v)).sort((a,b)=>a-b), reserve.filter(v=>!lost.includes(v)).sort((a,b)=>a-b)]
+    let res = n - lost.length
     
-    lost = lost.sort((a,b)=>a-b);
-    reserve = reserve.sort((a,b)=>a-b);
-    
-    let a = lost.filter(v=>!reserve.includes(v))
-    let b = reserve.filter(v=>!lost.includes(v))
-    
-    for(let i=0; i<b.length; i++){
-        reserveMap.set(b[i], 1);
-    }
-    
-    for(let i=0;i<a.length; i++){
-        if( reserveMap.get(a[i])===1 ){
-            reserveMap.set(a[i], 0);
-        }else if( reserveMap.get(a[i]-1)===1 ){
-            reserveMap.set(a[i]-1, 0);
-        }else if(reserveMap.get(a[i]+1)===1){
-            reserveMap.set(a[i]+1, 0);
-        }else{
-            lostMap.set(a[i], 1);
+    for(let i=0; i<lost.length; i++){
+        if(reserve.includes(lost[i]-1)){
+            res++
+            reserve = reserve.filter(v=>v!==lost[i]-1)
+        }else if(reserve.includes(lost[i]+1)){
+            res++
+            reserve = reserve.filter(v=>v!==lost[i]+1)
         }
     }
-    
-    return n - [...lostMap].length
+
+    return res
 }
