96bf2e39a46b2db602bb5079bc5c6a70f1190cfe
PUNGY <zkdvnd@naver.com>
2024-04-08 15:57:56 +0900

    [level 2] Title: 타겟 넘버, Time: 1.17 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,17 @@
 function solution(numbers, target) {
-    let res = 0;
-    function f(x, n = 0) {
-        if(n === numbers.length) {
-            if(x === target) res++;
-            return;
-        };
-
-        f(x + numbers[n], n + 1);
-        f(x - numbers[n], n + 1);
+    let count = 0;
+    function dfs(sum, depth){
+        if(depth === numbers.length){
+            if(sum === target){
+                count++
+            }
+            return
+        }
+        dfs(sum + numbers[depth], depth+1)
+        dfs(sum - numbers[depth], depth+1)
     }
-
-    f(numbers[0], 1)
-    f(numbers[0] * -1, 1)
-
-    return res;
+    
+    dfs(0,0)
+    
+    return count
 }
