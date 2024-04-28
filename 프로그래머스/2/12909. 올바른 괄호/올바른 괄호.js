4b8888a7aa666406fcf893c7981af1b164313ba5
PUNGY <zkdvnd@naver.com>
2024-04-28 13:25:27 +0900

    [level 2] Title: 올바른 괄호, Time: 25.77 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,8 @@
+function solution(s){
+    let count = 0;
+    for(let i=0; i<s.length; i++){
+        if (count<0) {return false}
+        s[i] === "(" ? count++ : count--;
+    }
+    return count === 0 ? true : false
+}
