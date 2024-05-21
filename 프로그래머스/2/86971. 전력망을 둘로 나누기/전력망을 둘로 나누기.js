6e51a075d3175203871e2d5c0aee04c17e7aba8d
PUNGY <zkdvnd@naver.com>
2024-05-22 02:32:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,44 +1,25 @@
 function solution(n, wires) {
-    const getGraph = (wires) => {
-        const graph = Array.from({ length: n + 1 }, () => []);
-        for (const [v1, v2] of wires) {
-            graph[v1].push(v2);
-            graph[v2].push(v1);
-        }
-        return graph;
-    };
-
-    const bfs = (graph, start, visited) => {
-        const queue = [start];
-        let count = 0;
-        while (queue.length > 0) {
-            const node = queue.shift();
-            if (!visited[node]) {
-                visited[node] = true;
-                count += 1;
-                for (const neighbor of graph[node]) {
-                    if (!visited[neighbor]) {
-                        queue.push(neighbor);
-                    }
-                }
-            }
-        }
-        return count;
-    };
-
-    let res = Infinity;
-
-    for (let i = 0; i < wires.length; i++) {
-        const remainingWires = wires.slice(0, i).concat(wires.slice(i + 1));
-        const graph = getGraph(remainingWires);
-
-        const visited = Array(n + 1).fill(false);
-
-        const count1 = bfs(graph, wires[i][0], visited);
-        const count2 = n - count1;
-
-        res = Math.min(res, Math.abs(count1 - count2));
+    const g=Array.from({length:n},()=>[]);
+    for(const e of wires){
+        g[e[0]-1].push(e[1]-1);
+        g[e[1]-1].push(e[0]-1);
     }
-
-    return res;
+    const p=new Array(n).fill(-1);
+    const q=[0];
+    for(let i=0;i<q.length;++i){
+        const u=q[i];
+        for(const v of g[u])if(v!=p[u]){
+            p[v]=u;
+            q.push(v);
+        }
+    }
+    let ans=n;
+    const dp=new Array(n).fill(1);
+    for(let i=q.length;--i>0;){
+        const v=q[i];
+        dp[p[v]]+=dp[v];
+        let a=Math.abs(n-2*dp[v]);
+        if(ans>a)ans=a;
+    }
+    return ans;
 }
