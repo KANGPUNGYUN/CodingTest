c72c472d7e7dbd893ca3a3b29055a1d2b777937b
PUNGY <zkdvnd@naver.com>
2024-05-25 13:07:34 +0900

    [level 3] Title: 정수 삼각형, Time: 4.63 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,10 @@
+function solution(triangle) {
+    for(let i=1; i<triangle.length; i++){
+        triangle[i][0] = triangle[i][0] + triangle[i-1][0]
+        for(let j=1; j<triangle[i].length-1; j++){
+            triangle[i][j] = Math.max(triangle[i-1][j-1] + triangle[i][j], triangle[i-1][j] + triangle[i][j]);
+        }
+        triangle[i][i] = triangle[i][i] + triangle[i-1][i-1]
+    }
+    return Math.max(...triangle.pop())
+}
