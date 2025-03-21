268943997d4fd33a7e35a4be8540e1b992049da4
PUNGY <zkdvnd@naver.com>
2025-03-21 22:05:26 +0900

    [level 2] Title: 가장 큰 수, Time: 172.51 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.06 ms
+메모리: 45 MB, 시간: 172.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 22:36:24
+2025년 03월 21일 22:05:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,11 +1,11 @@
 function solution(numbers) {
-    let res = [];
-    for(let number of numbers)
-        res.push(number.toString());
-
-    res.sort((a,b)=>{
-        return (b+a)-(a+b);
-    });
-
-    return res.join("")[0] == "0" ? "0" : res.join("");
+    let sorted = numbers.map(i=>i.toString()).sort((a, b) => {
+            return (b + a) - (a + b);
+        });
+    
+    if (sorted[0] === '0') {
+        return '0';
+    }
+    
+    return sorted.join("")
 }
